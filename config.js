6ea02c145fc667e8c6dfd5cdf7f580be00b1639a
NO9TA <54318994+NO9TA@users.noreply.github.com>
2020-11-11 11:53:52 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  TOKEN: "NTg3NzA1MzAzMjI3OTU3MjUz.XP6dNw.lSQnMJYRQQgHV3zxM0TRHv3AinI", // توكن بوتك
+  TOKEN: "", // توكن بوتك
   YT_API_KEY: "",
   prefix: "$", //برفكس البوت الأساسي
   dev : "565114619471986689" //أيديك
